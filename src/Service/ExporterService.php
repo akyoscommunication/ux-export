2159ee68dec60b3364b9fdcc191e86a5d3188575
akyoscommunication <webmaster.akyos@gmail.com>
2025-06-06 10:30:04 +0200

    feat: export related entities

--- src/Service/ExporterService.php
+++ src/Service/ExporterService.php
@@ -6,6 +6,7 @@ use Akyos\UXExportBundle\Attribute\ExportableProperty;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\BaseWriter;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 use Symfony\Component\PropertyAccess\PropertyAccess;
 
 class ExporterService
@@ -20,9 +21,21 @@ class ExporterService
 
     public function generateMatrix(Spreadsheet $spreadsheet, array $properties): void
     {
-        foreach ($properties as $index => $property) {
-            $name = $this->getPropertyName($property);
-            $spreadsheet->getActiveSheet()->setCellValue([$index + 1, 1], $name);
+        $columnIndex = 1;
+        foreach ($properties as $property) {
+            $attribute = $this->getAttribute($property);
+
+            if ($attribute && $attribute->manyToMany === ExportableProperty::MODE_SHEET) {
+                continue;
+            }
+
+            $fields = $attribute?->fields ?? [null];
+
+            foreach ($fields as $field) {
+                $name = $this->getPropertyName($property, $field);
+                $spreadsheet->getActiveSheet()->setCellValue([$columnIndex, 1], $name);
+                $columnIndex++;
+            }
         }
     }
 
@@ -30,23 +43,126 @@ class ExporterService
     {
         $propertyAccessor = PropertyAccess::createPropertyAccessor();
 
-        foreach ($data as $rowIndex => $item) {
-            foreach ($properties as $colIndex => $property) {
-                $value = $propertyAccessor->getValue($item, $property->getName());
-                $spreadsheet->getActiveSheet()->setCellValue([$colIndex + 1, $rowIndex + 2], $value);
+        $rowIndex = 2;
+        foreach ($data as $item) {
+            $lines = $this->getLinesCount($item, $properties, $propertyAccessor);
+            for ($line = 0; $line < $lines; $line++) {
+                $columnIndex = 1;
+                foreach ($properties as $property) {
+                    $attribute = $this->getAttribute($property);
+
+                    if ($attribute && $attribute->manyToMany === ExportableProperty::MODE_SHEET) {
+                        continue;
+                    }
+
+                    $value = $propertyAccessor->getValue($item, $property->getName());
+
+                    if ($attribute && $attribute->manyToMany === ExportableProperty::MODE_LINES) {
+                        $value = ($value[$line] ?? null);
+                    }
+
+                    $values = $this->extractValues($value, $attribute?->fields, $propertyAccessor);
+
+                    foreach ($values as $val) {
+                        $spreadsheet->getActiveSheet()->setCellValue([$columnIndex, $rowIndex], $val);
+                        $columnIndex++;
+                    }
+                }
+                $rowIndex++;
             }
         }
+
+        $this->populateManyToManySheets($spreadsheet, $data, $properties, $propertyAccessor);
     }
 
-    private function getPropertyName(\ReflectionProperty|\ReflectionMethod $property): string
+    private function getPropertyName(\ReflectionProperty|\ReflectionMethod $property, ?string $subField = null): string
     {
-        $attributeClass = ExportableProperty::class;
-        $attributes = $property->getAttributes($attributeClass);
+        $attribute = $this->getAttribute($property);
 
-        if (!empty($attributes) && $attributes[0]->newInstance()->name) {
-            return $attributes[0]->newInstance()->name;
+        $name = $attribute?->name ?? $property->getName();
+
+        if ($subField) {
+            return sprintf('%s_%s', $name, $subField);
         }
 
-        return $property->getName();
+        return $name;
+    }
+
+    private function getAttribute(\ReflectionProperty|\ReflectionMethod $property): ?ExportableProperty
+    {
+        $attributes = $property->getAttributes(ExportableProperty::class);
+
+        return !empty($attributes) ? $attributes[0]->newInstance() : null;
+    }
+
+    private function extractValues(mixed $value, ?array $fields, $accessor): array
+    {
+        if ($fields) {
+            $vals = [];
+            foreach ($fields as $field) {
+                $vals[] = $value ? $accessor->getValue($value, $field) : null;
+            }
+
+            return $vals;
+        }
+
+        return [$value];
+    }
+
+    private function getLinesCount(mixed $item, array $properties, $accessor): int
+    {
+        $max = 1;
+        foreach ($properties as $property) {
+            $attribute = $this->getAttribute($property);
+            if ($attribute && $attribute->manyToMany === ExportableProperty::MODE_LINES) {
+                $collection = $accessor->getValue($item, $property->getName());
+                $count = 0;
+                if (is_iterable($collection)) {
+                    foreach ($collection as $_) {
+                        $count++;
+                    }
+                }
+                $max = max($max, $count);
+            }
+        }
+
+        return $max;
+    }
+
+    private function populateManyToManySheets(Spreadsheet $spreadsheet, array $data, array $properties, $accessor): void
+    {
+        foreach ($properties as $property) {
+            $attribute = $this->getAttribute($property);
+            if (!$attribute || $attribute->manyToMany !== ExportableProperty::MODE_SHEET) {
+                continue;
+            }
+
+            $sheet = new Worksheet($spreadsheet, $this->getPropertyName($property));
+            $spreadsheet->addSheet($sheet);
+
+            $headerIndex = 1;
+            $sheet->setCellValue([1, 1], 'row');
+            foreach ($attribute->fields ?? [] as $field) {
+                $sheet->setCellValue([$headerIndex + 1, 1], $field);
+                $headerIndex++;
+            }
+
+            $rowIndex = 2;
+            foreach ($data as $i => $item) {
+                $collection = $accessor->getValue($item, $property->getName());
+                if (!is_iterable($collection)) {
+                    continue;
+                }
+                foreach ($collection as $element) {
+                    $sheet->setCellValue([1, $rowIndex], $i + 1);
+                    $colIndex = 2;
+                    foreach ($attribute->fields ?? [] as $field) {
+                        $sheet->setCellValue([$colIndex, $rowIndex], $accessor->getValue($element, $field));
+                        $colIndex++;
+                    }
+                    $rowIndex++;
+                }
+            }
+        }
     }
 }
